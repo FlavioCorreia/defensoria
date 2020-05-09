6280ec46170ee81f305396134435dd367fc03fbc
FlavioCorreia <32444541+FlavioCorreia@users.noreply.github.com>
2020-05-09 05:08:43 -0300

    Novos campos e atualizações
    Foram criados novos campos segundo o documento disponibilizado pela promotoria.
    Ainda faltam as verificações dos novos campos.

--- defensoria/lib/formulario.dart
+++ defensoria/lib/formulario.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class Formulario extends StatefulWidget {
@@ -8,13 +9,31 @@ class Formulario extends StatefulWidget {
 }
 
 class _FormularioState extends State<Formulario> {
-  //CONTROLADORES E BOOLEANOS DE VALIDAÇÃO
+  //CONTROLADORES DOS CAMPOS DE ENTRADA E BOOLEANOS DE VALIDAÇÃO DOS MESMOS
   final _cNomeMae = new TextEditingController();            bool _valNomeMae = true;
+  final _cNacionalidadeMae = new TextEditingController();   bool _valNacionalidadeMae = true;
+  final _cEstadoCivilMae = new TextEditingController();     bool _valEstadoCivilMae = true;
+  final _cProfissaoMae = new TextEditingController();       bool _valProfissaoMae = true;
+  final _cRGMae = new TextEditingController();              bool _valRGMae = true;
   final _cCPFMae = new TextEditingController();             bool _valCPFMae = true;
-  final _cFoneMae = new TextEditingController();            bool _valFoneMae = true;
+  final _cEnderecoMae = new TextEditingController();        bool _valEnderecoMae = true;
+  final _cNumeroCasaMae = new TextEditingController();      bool _valNumeroCasaMae = true;
+  final _cPontoReferenciaMae = new TextEditingController(); bool _valPontoReferenciaMae = true;
+  final _cBairroMae = new TextEditingController();          bool _valBairroMae = true;
+  final _cCidadeMae = new TextEditingController();          bool _valCidadeMae = true;
+
   final _cNomePai = new TextEditingController();            bool _valNomePai = true;
+  final _cNacionalidadePai = new TextEditingController();   bool _valNacionalidadePai = true;
+  final _cEstadoCivilPai = new TextEditingController();     bool _valEstadoCivilPai = true;
+  final _cProfissaoPai = new TextEditingController();       bool _valProfissaoPai = true;
+  final _cRGPai = new TextEditingController();              bool _valRGPai = true;
   final _cCPFPai = new TextEditingController();             bool _valCPFPai = true;
   final _cEnderecoPai = new TextEditingController();        bool _valEnderecoPai = true;
+  final _cNumeroCasaPai = new TextEditingController();      bool _valNumeroCasaPai = true;
+  final _cPontoReferenciaPai = new TextEditingController(); bool _valPontoReferenciaPai = true;
+  final _cBairroPai = new TextEditingController();          bool _valBairroPai = true;
+  final _cCidadePai = new TextEditingController();          bool _valCidadePai = true;
+
   final _cNomeCrianca = new TextEditingController();        bool _valNomeCrianca = true;
   final _cCPFCrianca = new TextEditingController();         bool _valCPFCrianca = true;
   final _cDataCrianca = new TextEditingController();        bool _valDataCrianca = true;
@@ -41,25 +60,70 @@ class _FormularioState extends State<Formulario> {
         padding: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
+            Divider(height: 10, color: Colors.white),
+            Text('Informações Do(s) Filho(s)', style: TextStyle(fontSize: 25)),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Nome Completo", "Nome: ", _cNomeCrianca, TextInputType.text, _valNomeCrianca),
+            Align(
+              alignment: Alignment.centerRight,
+              child: RaisedButton( child: Text("ADICIONAR"), textColor: Colors.white, color: Colors.blue,
+                  onPressed: (){}
+              ),
+            ),
+
+            Divider(height: 10, color: Colors.white),
+            Divider(height: 35, color: Colors.blue),
+
+            Divider(height: 10, color: Colors.white),
+            Text('Informações Da Mãe', style: TextStyle(fontSize: 25)),
+            Divider(height: 10, color: Colors.white),
             buildTextField("Nome Completo Da Mãe", "Nome: ", _cNomeMae, TextInputType.text, _valNomeMae),
             Divider(height: 10, color: Colors.white),
-            buildTextFieldM("CPF Da Mãe", "CPF: ", _cCPFMae, TextInputType.number, _valCPFMae, mask: maskCPFMae),
+            buildTextField("Nacionalidade", "", _cNacionalidadeMae, TextInputType.text, _valNacionalidadeMae),
             Divider(height: 10, color: Colors.white),
-            buildTextFieldM("Contato Da Mãe", "Telefone: ", _cFoneMae, TextInputType.phone, _valFoneMae, mask: maskFone),
+            buildTextFieldM("Estado Civil", "", _cEstadoCivilMae, TextInputType.text, _valEstadoCivilMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Profissão", "", _cProfissaoMae, TextInputType.text, _valProfissaoMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("RG", "", _cRGMae, TextInputType.number, _valRGMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextFieldM("CPF", "", _cCPFMae, TextInputType.number, _valCPFMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Endereço", "Rua: ", _cEnderecoMae, TextInputType.text, _valEnderecoMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Número Da Casa", "Nº: ", _cNumeroCasaMae, TextInputType.number, _valNumeroCasaMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Ponto De Referência", "", _cPontoReferenciaMae, TextInputType.text, _valPontoReferenciaMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Bairro", "", _cBairroMae, TextInputType.text, _valBairroMae),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Cidade", "", _cCidadeMae, TextInputType.text, _valCidadeMae),
 
             Divider(height: 35, color: Colors.blue),
+            Divider(height: 10, color: Colors.white),
+            Text('Informações Do Pai', style: TextStyle(fontSize: 25)),
+            Divider(height: 10, color: Colors.white),
             buildTextField("Nome Completo Do Pai", "Nome: ", _cNomePai, TextInputType.text, _valNomePai),
             Divider(height: 10, color: Colors.white),
-            buildTextFieldM("CPF Do Pai", "CPF: ", _cCPFPai, TextInputType.number, _valCPFPai, mask: maskCPFPai),
+            buildTextField("Nacionalidade", "", _cNacionalidadePai, TextInputType.text, _valNacionalidadePai),
             Divider(height: 10, color: Colors.white),
-            buildTextField("Endereço Do Pai", "End.: ", _cEnderecoPai, TextInputType.text, _valEnderecoPai),
-
-            Divider(height: 35, color: Colors.blue),
-            buildTextField("Nome Completo Da Criança", "Nome: ", _cNomeCrianca, TextInputType.text, _valNomeCrianca),
+            buildTextFieldM("Estado Civil", "", _cEstadoCivilPai, TextInputType.text, _valEstadoCivilPai),
             Divider(height: 10, color: Colors.white),
-            buildTextFieldM("CPF Da Criança", "CPF: ", _cCPFCrianca, TextInputType.number, _valCPFCrianca, mask: maskCPFFilho),
+            buildTextField("Profissão", "", _cProfissaoPai, TextInputType.text, _valProfissaoPai),
             Divider(height: 10, color: Colors.white),
-            buildTextFieldM("Nascimento Da Criança", "Data: ", _cDataCrianca, TextInputType.datetime, _valDataCrianca, mask: maskData),
+            buildTextField("RG", "", _cRGPai, TextInputType.number, _valRGPai),
+            Divider(height: 10, color: Colors.white),
+            buildTextFieldM("CPF", "", _cCPFPai, TextInputType.number, _valCPFPai),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Endereço", "Rua: ", _cEnderecoPai, TextInputType.text, _valEnderecoPai),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Número Da Casa", "Nº: ", _cNumeroCasaPai, TextInputType.number, _valNumeroCasaPai),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Ponto De Referência", "Referência: ", _cPontoReferenciaPai, TextInputType.text, _valPontoReferenciaPai),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Bairro", "", _cBairroPai, TextInputType.text, _valBairroPai),
+            Divider(height: 10, color: Colors.white),
+            buildTextField("Cidade", "", _cCidadePai, TextInputType.text, _valCidadePai),
 
             Divider(height: 35, color: Colors.blue),
             RaisedButton(
@@ -67,7 +131,6 @@ class _FormularioState extends State<Formulario> {
                 setState(() { // SE ALGUM CAMPO ESTIVER VAZIO OU INCOMPLETO, TORNE-O INVALIDO
                  _cNomeMae.text.isEmpty ? _valNomeMae = false : _valNomeMae = true;
                  _cCPFMae.text.length != 14  ? _valCPFMae = false : _valCPFMae = true; // 11 CPF 2 . 1 -
-                 _cFoneMae.text.length != 16 ? _valFoneMae = false : _valFoneMae = true; // () - ' ' => 5
                  _cNomePai.text.isEmpty ? _valNomePai = false : _valNomePai = true;
                  _cCPFPai.text.length != 14  ? _valCPFPai = false : _valCPFPai = true;
                  _cEnderecoPai.text.isEmpty ? _valEnderecoPai = false : _valEnderecoPai  = true;
@@ -76,9 +139,8 @@ class _FormularioState extends State<Formulario> {
                  _cDataCrianca.text.length != 10 ? _valDataCrianca = false : _valDataCrianca = true; // // => 2
                 });
 
-                if(_valNomeMae && _valCPFMae && _valFoneMae && _valNomePai && _valCPFPai &&
-                    _valEnderecoPai && _valNomeCrianca && _valCPFCrianca && _valDataCrianca){
-                  //SE TODOS OS CAMPOS ESTIVEREM CORRETOS
+                if(_valNomeMae && _valCPFMae  && _valNomePai && _valCPFPai && _valEnderecoPai
+                    && _valNomeCrianca && _valCPFCrianca && _valDataCrianca){//SE TODOS OS CAMPOS ESTIVEREM CORRETOS
                   _showDialog(context, "", "Formulário enviado com sucesso.");
                 }else{
                   _showDialog(context, "Campos Incorretos", "Alguns campos não foram preenchidos corretamente, reinforme os campos destacados.");
@@ -120,6 +182,7 @@ Widget buildTextField(String label, String prefix, TextEditingController c, Text
 //PASSANDO A MÁSCARA
 Widget buildTextFieldM(String label, String prefix, TextEditingController c, TextInputType tit, bool validator, {MaskTextInputFormatter mask, Function f}){
   return TextField(
+    scrollPadding: EdgeInsets.fromLTRB(30, 30, 30, 30),
     controller: c,
     inputFormatters: [mask],
     decoration: InputDecoration(
